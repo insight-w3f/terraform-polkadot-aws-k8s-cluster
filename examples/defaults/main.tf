5df1f9cd079e7113d689d4e16b25eb6feeb2816f
rob <robcannonxyz@gmail.com>
2020-04-19 18:47:01 -0700

    moving k8s provider into module

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -6,22 +6,6 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "1.10"
-}
-
 resource "random_pet" "this" {
   length = 2
 }
@@ -30,7 +14,7 @@ module "network" {
   source       = "github.com/insight-w3f/terraform-polkadot-aws-network.git?ref=master"
   num_azs      = 3
   cluster_name = random_pet.this.id
-  k8s_enabled = true
+  k8s_enabled  = true
 }
 
 module "eks" {
