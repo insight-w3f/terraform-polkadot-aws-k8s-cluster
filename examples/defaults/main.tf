577d557387f40d7c96216b1ff56e204f35799f56
rob <robcannonxyz@gmail.com>
2020-05-11 01:58:32 -0700

    remove cluster_name in test

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -22,20 +22,20 @@ provider "kubernetes" {
   version                = "1.10"
 }
 
-resource "random_pet" "this" {
-  length = 2
-}
+//resource "random_pet" "this" {
+//  length = 2
+//}
 
 module "network" {
-  source       = "github.com/insight-w3f/terraform-polkadot-aws-network.git?ref=master"
-  num_azs      = 3
-  cluster_name = random_pet.this.id
+  source  = "github.com/insight-w3f/terraform-polkadot-aws-network.git?ref=master"
+  num_azs = 3
+  //  cluster_name = random_pet.this.id
   k8s_enabled = true
 }
 
 module "eks" {
-  source            = "../.."
-  cluster_name      = random_pet.this.id
+  source = "../.."
+  //  cluster_name      = random_pet.this.id
   security_group_id = module.network.k8s_security_group_id
   subnet_ids        = slice(module.network.public_subnets, 0, 3)
   vpc_id            = module.network.vpc_id
